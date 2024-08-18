f7df9e0b8c06954b278dea1220a5ac4a0a9a46f4
Jit Debnath <jit.nathdeb@gmail.com>
2024-08-18 20:00:47 +0530

    made the dream possible

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ function app() {
 
     return (
         <>
-            <h1 className="text-center color-white-400">heading</h1>
-            <ul className="felx row-auto gap-2">
-                <li className="bg-slate-600">home</li>
-                <li className="bg-slate-600">about</li>
-                <li className="bg-slate-600">contact</li>
+            <h1>heading</h1>
+            <ul>
+                <li>home</li>
+                <li>about</li>
+                <li>contact</li>
             </ul>
         </>
     );
 }
+
+export default app
